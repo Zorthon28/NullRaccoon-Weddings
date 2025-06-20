75456ee8dabe9ef2930f9ee21d671ae49c6b5d80
Zorthon28 <99435781+Zorthon28@users.noreply.github.com>
2025-06-20 02:10:28 -0700

    Checstyle

--- wedding-site/app/routes/family/page.tsx
+++ wedding-site/app/routes/family/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 export default function FamilyPage() {
   return (
     <Hero
-      coupleNames={["Kenia &", "Gustavo"]}
+      coupleNames={["Kenia &", " Gustavo"]}
       eventDate={new Date("2025-07-04T00:00:00")} // ← Updated here
       message="¡Nos casamos el 4 de julio de 2025! 🎉"
     />
